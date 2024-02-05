9fbb4dd8869c4063dff2c78222e078fe93df72b6
Cinos Codes <mankame96@gmail.com>
2024-02-06 01:00:48 +0530

    Added Navbar along with Responsiveness

---
+++ frontend_react/src/container/Skills/skills.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Skills.scss'
+
+const Skills = () => {
+  return (
+    <div>skills</div>
+  )
+}
+
+export default Skills;
---
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Testimonial.scss'
+
+const Testimonial = () => {
+  return (
+    <div>testimonials</div>
+  )
+}
+
+export default Testimonial;
